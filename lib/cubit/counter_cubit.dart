dd356eb047b73608a18b76f5c7b8e9fe7f45a209
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-20 20:18:30 +0300

    create logic and cubit method

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:basketball_counter/cubit/counter_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterStates> {
+  CounterCubit() : super(IntialState());
+  int teamA = 0;
+  int teamB = 0;
+  incrementTeam({required String teamName, required int num}) {
+    if (teamName == 'A') {
+      teamA += num;
+      emit(TeamAincrement());
+    } else {
+      teamB += num;
+      emit(TeamBincrement());
+    }
+  }
+}
---
+++ lib/cubit/counter_states.dart
@@ -0,0 +1,5 @@
+abstract class CounterStates {}
+class IntialState extends CounterStates{}
+class TeamAincrement extends CounterStates{}
+class TeamBincrement extends CounterStates{}
+class ResetCounter extends CounterStates{}
